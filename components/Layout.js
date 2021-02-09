25206bf8c5adb46fd8862e3cf7a5323ccbf3606d
venaz-dev <chibuzo.madu90@gmail.com>
2021-02-09 15:16:15 +0100

    ogimage

--- components/Layout.js
+++ components/Layout.js
@@ -15,14 +15,6 @@ const Layout = ({ page, children }) => {
   };
   return (
     <div>
-      <Head>
-        <title>Venaz Portfolio</title>
-        <link rel="icon" href="/favicon.png" />
-        <meta
-          property="og:image"
-          content="https://techietainment.io/wp-content/uploads/2021/02/State-of-cryptocurrencies-in-Nigeria.jpg"
-        ></meta>
-      </Head>
       <Nav page={page} loading={toogleLoading} />
       <div className="children">{children}</div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,53 @@
 import "../styles/globals.scss";
 import { motion, AnimatePresence } from "framer-motion";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps, router }) {
   return (
-    <AnimatePresence exitBeforeEnter>
-      <motion.div
-        key={router.route}
-        initial="pageInitial"
-        animate="pageAnimate"
-        exit="pageExit"
-        // transition={{ duration: 0.5,}}
-        variants={{
-          pageInitial: {
-            // backgroundColor: '#444444',
-            // transform: ' translateY(-100vh)'
-            opacity: 0,
-            // filter: 'invert()',
-            // transform: 'translateX(-100vw)',
-            
-            
-          },
-          pageAnimate: {
-            opacity: 1,
-            
-            // transform: ' translateY(0vh)',
-            // backgroundColor: '#101010',
-          },
-          pageExit:{
-            // backgroundColor: '#444444',
-            // filter: 'invert()',
-            opacity: 0,
-            // transform: ' translateY(100vh)'
-          }
-        }}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
+    <>
+      <Head>
+        <title>Venaz Portfolio</title>
+        <link rel="icon" href="/favicon.png" />
+        <meta
+          property="og:image"
+          content="https://techietainment.io/wp-content/uploads/2021/02/State-of-cryptocurrencies-in-Nigeria.jpg"
+        />
+        <meta property="og:image:height" content="600" />
+        <meta property="og:image:width" content="900" />
+      </Head>
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={router.route}
+          initial="pageInitial"
+          animate="pageAnimate"
+          exit="pageExit"
+          // transition={{ duration: 0.5,}}
+          variants={{
+            pageInitial: {
+              // backgroundColor: '#444444',
+              // transform: ' translateY(-100vh)'
+              opacity: 0,
+              // filter: 'invert()',
+              // transform: 'translateX(-100vw)',
+            },
+            pageAnimate: {
+              opacity: 1,
+
+              // transform: ' translateY(0vh)',
+              // backgroundColor: '#101010',
+            },
+            pageExit: {
+              // backgroundColor: '#444444',
+              // filter: 'invert()',
+              opacity: 0,
+              // transform: ' translateY(100vh)'
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </>
   );
 }
 
